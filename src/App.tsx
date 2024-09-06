afe7040cfab16f1915d3647f13e52e799e3c5bf9
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-09-06 12:13:22 +0900

    Button Basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { Button, Typography } from "@mui/material";
 
 function App() {
-  return (
-    <>
-      {/* <App /> */}
-      <Button>Hello World</Button>
-      <Button variant="dashed">Hello World</Button>
-      <Typography variant="h4" color="secondary">
-        Hello secondary
-      </Typography>
-      <Button variant="contained">Contained</Button>
-      <Button variant="contained" disabled>
-        Disabled
-      </Button>
-      <Button variant="contained" href="#contained-buttons">
-        Link
-      </Button>
-    </>
-  );
+	return (
+		<>
+			{/* <App /> */}
+			<Button>Hello World</Button>
+			<Button variant="dashed">Hello World</Button>
+			<Typography variant="h4" color="secondary">
+				Hello secondary
+			</Typography>
+			<Button>Contained</Button>
+			<Button variant="contained" disabled>
+				Disabled
+			</Button>
+			<Button variant="contained" href="#contained-buttons">
+				Link
+			</Button>
+		</>
+	);
 }
 export default App;
--- src/theme.ts
+++ src/theme.ts
@@ -1,91 +1,112 @@
-import { createTheme } from "@mui/material/styles";
+import { blue } from "@mui/material/colors";
 
 declare module "@mui/material/styles" {
-  interface Theme {
-    status: {
-      danger: string;
-    };
-  }
-  // allow configuration using `createTheme`
-  interface ThemeOptions {
-    status?: {
-      danger?: string;
-    };
-    cssVariables?: boolean;
-    cssVarPrefix?: string;
-    defaultColorScheme?: "light" | "dark" | "system";
-  }
+	interface Theme {
+		status: {
+			danger: string;
+		};
+	}
+	// allow configuration using `createTheme`
+	interface ThemeOptions {
+		status?: {
+			danger?: string;
+		};
+		cssVariables?: boolean;
+		cssVarPrefix?: string;
+		defaultColorScheme?: "light" | "dark" | "system";
+	}
 }
 
 declare module "@mui/material/Button" {
-  interface ButtonPropsVariantOverrides {
-    dashed: true;
-  }
+	interface ButtonPropsVariantOverrides {
+		dashed: true;
+	}
 }
 
 const theme = {
-  cssVariables: true,
-  cssVarPrefix: "origin",
-  defaultColorScheme: "light",
-  status: {
-    danger: "#e53e3e",
-  },
-  colorSchemes: {
-    light: {
-      palette: {
-        primary: {
-          main: "#e96812",
-        },
-        secondary: {
-          main: "#564962",
-        },
-        background: {
-          default: "#fff",
-          paper: "#f5f5f5",
-        },
-        text: {
-          primary: "#000",
-          secondary: "#555",
-        },
-      },
-    },
-    // dark: true,
-    dark: {
-      palette: {
-        primary: {
-          main: "#e507c7",
-        },
-        secondary: {
-          main: "#7eb6ea",
-        },
-        background: {
-          default: "#262626",
-        },
-        text: {
-          primary: "#fff",
-          secondary: "#aaa",
-        },
-      },
-    },
-  },
-  // components
-  components: {
-    MuiButtonBase: {
-      defaultProps: {
-        disableElevation: true,
-        disableRipple: true,
-      },
-      styleOverrides: {
-        root: {
-          variants: [
-            {
-              props: { variant: "outlined" },
-            },
-          ],
-        },
-      },
-    },
-  },
+	cssVariables: true,
+	cssVarPrefix: "origin",
+	defaultColorScheme: "light",
+	status: {
+		danger: "#e53e3e",
+	},
+	colorSchemes: {
+		light: {
+			palette: {
+				primary: {
+					main: "#e96812",
+				},
+				secondary: {
+					main: "#564962",
+				},
+				background: {
+					default: "#fff",
+					paper: "#f5f5f5",
+				},
+				text: {
+					primary: "#000",
+					secondary: "#555",
+				},
+			},
+		},
+		// dark: true,
+		dark: {
+			palette: {
+				primary: {
+					main: "#e507c7",
+				},
+				secondary: {
+					main: "#7eb6ea",
+				},
+				background: {
+					default: "#262626",
+				},
+				text: {
+					primary: "#fff",
+					secondary: "#aaa",
+				},
+			},
+		},
+	},
+	// components
+	components: {
+		MuiButtonBase: {
+			defaultProps: {
+				disableRipple: true,
+			},
+			styleOverrides: {
+				root: {
+					variants: [
+						{
+							props: { variant: "outlined" },
+							style: {
+								textTransform: "none",
+							},
+						},
+					],
+				},
+			},
+		},
+		MuiButton: {
+			defaultProps: {
+				disableElevation: true,
+				variant: "contained",
+			},
+			styleOverrides: {
+				root: {
+					variants: [
+						{
+							props: { variant: "dashed" },
+							style: {
+								textTransform: "none",
+								border: `2px dashed ${blue[500]}`,
+							},
+						},
+					],
+				},
+			},
+		},
+	},
 };
 
 export default theme;
